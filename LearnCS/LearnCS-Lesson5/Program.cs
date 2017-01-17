75db3b0cf6693297d3eef96ca5e03874f9fa271d
DanielMChaves <daniel.melero.95@gmail.com>
2017-01-17 13:48:00 +0100

    Lesson 1 to 6

---
+++ LearnCS/LearnCS-Lesson5/Program.cs
@@ -0,0 +1,164 @@
+﻿/*
+ * Trabajando con Decisiones
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5
+{
+    public class Program
+    {
+
+        /*
+        * Ejercicio:
+        *  Generar un número aleatorio y que el usuaio sepa cual es
+        */
+        static void Exercise()
+        {
+
+            int correctNumber = new Random().Next(4);
+            Console.WriteLine("Escribe un número entre el 1 y el 3");
+            int guessedNumber = int.Parse(Console.ReadLine());
+
+            if (correctNumber.Equals(guessedNumber))
+            {
+                Console.WriteLine("Correcto!");
+                Console.WriteLine("Has acertado el número");
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto!");
+                Console.WriteLine("Has fallado el número");
+            }
+
+        }
+
+        static void Greet(string name)
+        {
+            if (String.IsNullOrEmpty(name)) return;
+
+            Console.WriteLine($"Hello, {name}!");
+
+        }
+
+        static int Compare(int x, int y)
+        {
+
+            if (x < y)
+            {
+                return -1;
+            }
+            else
+            {
+                if (x == y)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+
+        }
+
+        static int Compare2(int x, int y)
+        {
+
+            if (x < y)
+            {
+                return -1;
+            }
+            else if (x == y)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+
+        }
+
+        static void Switch(uint guessedNumber)
+        {
+
+            switch (guessedNumber)
+            {
+                case 0:
+                    Console.WriteLine("Lo siento, 0 no es un nº válido.");
+                    break;
+                case 1:
+                case 2:
+                case 3:
+                    Console.WriteLine("Elegiste un número inferior.");
+                    break;
+                case 4:
+                    Console.WriteLine("Elegiste el número correcto!");
+                    break;
+                default:
+                    Console.WriteLine("Elegiste un número superior.");
+                    break;
+            }
+
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Lesson 5");
+
+            /*
+             * Sentencia IF y ELSE
+             */
+
+            Console.WriteLine("¿Cuál es la capital de Ohio?");
+            var answer = Console.ReadLine();
+            if (answer.ToLower() == "columbus")
+            {
+                Console.WriteLine("Correcto!");
+            }
+            else
+            {
+                Console.WriteLine("Lo siento, esa no es la respuesta correcta.");
+            }
+
+            // Anidamientos
+
+            var resultCompare1 = Compare(1, 2);
+            Console.WriteLine($"Resultado de la comparación (1): {resultCompare1}");
+
+            var resultCompare2 = Compare2(2, 1);
+            Console.WriteLine($"Resultado de la comparación (2): {resultCompare2}");
+
+            /*
+             * Sentencia SWITCH
+             */
+
+            // El tipo uint es para numeros enteros positivos
+            // Switch(uint guessedNumber)
+
+            uint guessedNumber = 10;
+            Switch(guessedNumber);
+
+            /*
+             * Anexo
+             */
+
+            // Obtener un número randow del 1 al 3
+            // int correctNumber = new Random().Next(4);
+
+            // Leer de la linea de comandos un número
+            // int guessedNumber = int.Parse(Console.ReadLine());
+
+            // Ejercicio
+            Exercise();
+
+            Console.WriteLine("Final Lesson 5");
+            Console.Read();
+        }
+    }
+}
---
+++ LearnCS/LearnCS-Lesson7/Program.cs
@@ -0,0 +1,109 @@
+﻿/*
+ * Expresiones Lógicas y Bucles
+ */
+
+using System;
+
+namespace Lesson7
+{
+    public class Program
+    {
+
+        /*
+        * Ejercicio:
+        */
+        static void Exercise()
+        {
+
+            Console.WriteLine("Escribe un número:");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Factor: ");
+
+            if (number > 1) // convert this to while
+            {
+                int candidateFactor = 2;
+
+                if (candidateFactor <= number) // convert this to while
+                {
+                    if (number % candidateFactor == 0) // found a factor
+                    {
+                        Console.Write(candidateFactor);
+                        // divide number by the factor you found and assign this back to number
+                        // print a comma if number is still greater than 1
+                    }
+                    // don't forget to increment factor!
+                }
+            }
+            Console.WriteLine();
+
+        }
+
+        static void BubleBasico()
+        {
+            // Generamos un número del 1 al 100
+            int numberToGuess1 = new Random().Next(1, 101);
+            // Empezamos con un número incorrecto
+            int currentGuess1 = 0;
+
+            while (currentGuess1 != numberToGuess1)
+            {
+                Console.WriteLine("Escribe un número (1 al 100): ");
+                currentGuess1 = int.Parse(Console.ReadLine());
+                if (currentGuess1 < numberToGuess1)
+                {
+                    Console.WriteLine("El número es muy bajo! Prueba otra vez!");
+                }
+                if (currentGuess1 > numberToGuess1)
+                {
+                    Console.WriteLine("El número es muy alto! Prueba otra vez!");
+                }
+            }
+            Console.WriteLine("Correcto!");
+        }
+
+        static void BucleInfinito()
+        {
+            // Generamos un número del 1 al 100
+            int numberToGuess2 = new Random().Next(1, 101);
+
+            // Al ser la condición 'true' siempre se ejecuta el bucle
+            while (true)
+            {
+                Console.WriteLine("Escribe un número (1 al 100): ");
+                int currentGuess2 = int.Parse(Console.ReadLine());
+
+                if (currentGuess2 == numberToGuess2) break;
+                if (currentGuess2 < numberToGuess2)
+                {
+                    Console.WriteLine("El número es muy bajo! Prueba otra vez!");
+                }
+                if (currentGuess2 > numberToGuess2)
+                {
+                    Console.WriteLine("El número es muy alto! Prueba otra vez!");
+                }
+            }
+            Console.WriteLine("Correcto!");
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Lesson 7");
+
+            /*
+             * Expresiones para Bucles
+             */
+
+            // Bucle Básico
+            //BubleBasico();
+
+            // Bucle Infinito
+            //BucleInfinito();
+
+            // Ejercicio
+            Exercise();
+
+            Console.WriteLine("Final Lesson 7");
+            Console.Read();
+        }
+    }
+}
